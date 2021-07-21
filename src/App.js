23861cabe7f7b0fc72f44a79ebc3b0e025b8251f
ishitakeshawani <keshwaniishita054@gmail.com>
2021-07-21 16:19:22 +0530

    implement UI for cash - register

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import {  useState } from 'react';
 import './App.css';
+import TakeInput from './takeInput';
+
 
 function App() {
+  const [amount,setAmount] = useState();
+  const [error,setError] = useState('');
+  const [showText, setShowText] = useState(false);
+  const [empty,setEmpty] = useState(true);
+
+  
+
+  function handleSubmit(){
+    if(amount === "" || amount === undefined){
+      setError("Enter valid bill amount");
+      setEmpty(true);
+      
+    }else{
+      setShowText(true);
+      setEmpty(false);
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+     <div className="container">
+        <h2 className="title">
+        Cash Register Manager
+        </h2>
+        <p className="about">
+        Enter the bill amount and cash given by the customer and know minimum number of notes to return.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          <div className="bill-amount">
+            Bill Amount:
+          </div>
+          <input type="number" id="bill" value={amount} 
+          onChange={(e) => {
+            setAmount(e.target.value);
+            }}
+          />
+         {empty ? <button onClick={handleSubmit}>Next</button> : "" }
+          { showText ? <TakeInput amount={amount} /> : <p className="about">{error  || ""}</p> }
+           
+          
+          
+        </div>
+        
+     </div>
     </div>
   );
 }
+
+
 
 export default App;
