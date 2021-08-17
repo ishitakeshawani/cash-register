7b9d6c2cd5b435f195ee754280cc77aede16007d
ishitakeshawani <keshwaniishita054@gmail.com>
2021-08-17 15:21:55 +0530

    add calculate  function

--- src/App.js
+++ src/App.js
@@ -4,51 +4,47 @@ import TakeInput from './takeInput';
 
 
 function App() {
-  const [amount,setAmount] = useState();
-  const [error,setError] = useState('');
+  const [amount, setAmount] = useState();
+  const [error, setError] = useState("");
   const [showText, setShowText] = useState(false);
-  const [empty,setEmpty] = useState(true);
+  const [empty, setEmpty] = useState(true);
 
-  
-
-  function handleSubmit(){
-    if(amount === "" || amount === undefined){
+  function handleSubmit() {
+    if (amount === "" || amount === undefined) {
       setError("Enter valid bill amount");
       setEmpty(true);
-      
-    }else{
+    } else {
       setShowText(true);
       setEmpty(false);
     }
   }
 
-
   return (
     <div className="App">
-     <div className="container">
-        <h2 className="title">
-        Cash Register Manager
-        </h2>
+      <div className="container">
+        <h2 className="title">Cash Register Manager</h2>
         <p className="about">
-        Enter the bill amount and cash given by the customer and know minimum number of notes to return.
+          Enter the bill amount and cash given by the customer and know minimum
+          number of notes to return.
         </p>
         <div>
-          <div className="bill-amount">
-            Bill Amount:
-          </div>
-          <input type="number" id="bill" value={amount} 
-          onChange={(e) => {
-            setAmount(e.target.value);
+          <div className="bill-amount">Bill Amount:</div>
+          <input
+            type="number"
+            id="bill"
+            value={amount}
+            onChange={(e) => {
+              setAmount(e.target.value);
             }}
           />
-         {empty ? <button onClick={handleSubmit}>Next</button> : "" }
-          { showText ? <TakeInput amount={amount} /> : <p className="about">{error  || ""}</p> }
-           
-          
-          
+          {empty ? <button onClick={handleSubmit}>Next</button> : ""}
+          {showText ? (
+            <TakeInput amount={amount} />
+          ) : (
+            <p className="about">{error || ""}</p>
+          )}
         </div>
-        
-     </div>
+      </div>
     </div>
   );
 }
--- src/takeInput.js
+++ src/takeInput.js
@@ -6,25 +6,44 @@ const TakeInput= (props) => {
     const [pay,setPay] = useState();
     let [error,setError] = useState('');
     const [iserror,setISError] = useState(true);
+    let notes = [2000,500,100,20,10,5,1];
+    let [ans,setans] = useState([]);
+
+
+  const calculate = (amountToreturn) => {
+    let note = Array(7).fill(0)
+   
+    for (let index = 0; index < notes.length; index++) {
+      const numofnotes = Math.trunc(
+        amountToreturn / notes[index]
+      );
+      
+      amountToreturn = amountToreturn % notes[index];
+      note[index] = numofnotes;
+      
+    }
+    setans(note)
+  }
+  // console.log(noofnotes[0]);
+
+
+
     function handleCheck(){
            
-
-            if(pay === "" || pay === undefined){
-                setISError(true);
-                setError("Enter valid bill amount and cash given to continue");
-            }else if(pay === props.amount){
-               setISError(true);
-                setError("No amount should be returned");
-                
-            }else if(pay < props.amount){
-               setISError(true);
-                setError("Cash is less than bill, please enter right amount");
-            }else{
-                setISError(false);
-                // setError(" ");
+            if (pay === "" || pay === undefined) {
+              setISError(true);
+              setError("Enter valid bill amount and cash given to continue");
+            } else if (Number(pay) === Number(props.amount)) {
+              setISError(true);
+              setError("No amount should be returned");
+            } else if (Number(pay) < Number(props.amount)) {
+              setISError(true);
+              setError("Cash is less than bill, please enter right amount");
+            } else if (Number(pay) > Number(props.amount)) {
+              const amountToreturn = Number(pay) - Number(props.amount);
+              calculate(amountToreturn);
+              setISError(false);
             }
-       
-
     }
 
 
@@ -33,17 +52,17 @@ const TakeInput= (props) => {
         <div>
            <div>
           <div className="bill-amount">
-            Cash Given:
           </div>
           <input type="number" id="pay" value={pay} 
-          onChange={(e) => {
+            onChange={(e) => {
             setPay(e.target.value);
             }}
           />
           </div>
           <button onClick={handleCheck}>Check</button>
-          {iserror ?  <p className="about">{error  || ""}</p> : <Results amount={props.amount} pay={pay}/> }
-          
+          {iserror ?  <p className="about">{error  || ""}</p> : <Results ans={ans}/> }
+         
+         
         </div>
     );
 }
